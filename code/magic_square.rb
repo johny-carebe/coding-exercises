f1b4df4b42f9b0809635a17f491e258480f72808
Johny Carebe <johny.carebe@gmail.com>
2023-10-23 20:16:31 -0300

    Solve magic square by sum of rows comparing with sum of columns

--- code/magic_square.rb
+++ code/magic_square.rb
@@ -1,7 +1,11 @@
 class MagicSquare
-  # Implement a method that takes a 2D array, 
+  # Implement a method that takes a 2D array,
   # checks if it's a magic square and returns either true or false depending on the result.
   # sequence (https://en.wikipedia.org/wiki/Magic_square)
   def self.validate(square)
+     sum_by_rows = square.map { |row| row.sum }
+     sum_by_columns = square.transpose.map { |column| column.sum }
+
+     sum_by_rows == sum_by_columns
   end
 end
