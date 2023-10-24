dcd624987003a63412742a20bca3e621a3800ab4
Johny Carebe <johny.carebe@gmail.com>
2023-10-23 21:56:59 -0300

    Solve Magic Square use cases

--- code/magic_square.rb
+++ code/magic_square.rb
@@ -4,7 +4,9 @@ class MagicSquare
     # checks if it's a magic square and returns either true or false depending on the result.
     # sequence (https://en.wikipedia.org/wiki/Magic_square)
     def validate(square)
-       all_sum_by_rows_are_equal(square) && all_sum_by_columns_are_equal(square)
+       all_sum_by_rows_are_equal(square) &&
+         all_sum_by_columns_are_equal(square) &&
+         all_diagonal_sums_are_equal(square)
     end
 
     private
@@ -29,6 +31,21 @@ class MagicSquare
 
     def sum_by_columns(square)
       square.transpose.map { |column| column.sum }
+    end
+
+    def all_diagonal_sums_are_equal(square)
+      square_length = square.length - 1
+      square_range = (0..square_length)
+
+      main_diagonal_sum = square_range.sum { |main_diagonal| square[main_diagonal][main_diagonal] }
+
+      secondary_diagonal_sum = square_range.sum do |column|
+        left_to_right_row = -column - 1
+
+        square[left_to_right_row][column]
+      end
+
+      main_diagonal_sum == secondary_diagonal_sum
     end
   end
 end
