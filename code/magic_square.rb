9614d056395adfe10eee73651ae35c8c8b014c72
Johny Carebe <johny.carebe@gmail.com>
2023-10-23 21:14:46 -0300

    Magic Square sums for all rows with same value and all columns with same value

--- code/magic_square.rb
+++ code/magic_square.rb
@@ -4,13 +4,27 @@ class MagicSquare
     # checks if it's a magic square and returns either true or false depending on the result.
     # sequence (https://en.wikipedia.org/wiki/Magic_square)
     def validate(square)
-       sum_by_rows(square) == sum_by_columns(square)
+       all_sum_by_rows_are_equal(square) && all_sum_by_columns_are_equal(square)
     end
 
     private
 
+    def all_sum_by_rows_are_equal(square)
+      sum_by_rows_list = sum_by_rows(square)
+      first_row_sum = sum_by_rows_list.first
+
+      sum_by_rows_list.all? { |row_sum| row_sum == first_row_sum }
+    end
+
     def sum_by_rows(square)
-      square.map { |row| row.sum }
+      a = square.map { |row| row.sum }
+    end
+
+    def all_sum_by_columns_are_equal(square)
+      sum_by_columns_list = sum_by_columns(square)
+      first_column_sum = sum_by_columns_list.first
+
+      sum_by_columns_list.all? { |column_sum| column_sum == first_column_sum }
     end
 
     def sum_by_columns(square)
